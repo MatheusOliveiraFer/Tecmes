88bca4ae1c0326b7fab37220cb685939d1af796a
Matheus Henrique <matheusoliveiraa97@hotmail.com>
2022-01-03 20:28:22 -0300

    Rotas de login e home configuradas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ export const Home = () => {
     return(
         <div>
             <h1>Página de Home</h1>
+            <a href="../index2.html"> Ir para Login</a>
         </div>
     );
 }
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import { Login } from '../pages/Login';
-
+import { Home } from '../pages/Home';
 
 export const MainRoutes = () => {
     return (
         <Routes>
-            <Route path="" element={<Login />} />
+            <Route path="/" element={<Home />} />
         </Routes>
     );
 }
