2a4d63db99e53b6ad2e259ce9102e47cebf8afee
Matheus Henrique <matheusoliveiraa97@hotmail.com>
2022-01-07 01:11:56 -0300

    Conclusão da tela de Produão

---
+++ src/pages/A.Producao/index.jsx
@@ -0,0 +1,58 @@
+import './style.css'
+
+export const AProducao = () => {
+    return(
+        // <div className="color">
+        //     <h1>Painel Ordem de Produção</h1>
+        //     <a href="../../login.html">Ir para login</a>
+        // </div>
+        <section>
+            <div className="content">
+                <div className="title_formulario">
+                        <div className="title_formulario_h1">Apontamento de Produção</div>
+                </div>
+                <form class="painel_OP">
+                    <div className="input_left">
+                            <div className="title">Número da Ordem:</div>
+                            <div className="N_Ordem">
+                                <input type="text" name="input" id="ordem" placeholder="Digite o número da ordem" required />
+                            </div>
+                            <div className="title">Cliente:</div>
+                            <div className="cliente">
+                                <input type="text" name="input" id="email"
+                                pattern=".+@globex\.com" size="30" placeholder="Digite nome do cliente" required />
+                            </div>
+                            <div className="title">Quantidade:</div>
+                            <div className="quantidade">
+                                <input type="number" name="input" id="quantidade" placeholder="Insira a quantidade" required />
+                            </div>
+                    </div>
+                    <div className="input_right">
+                        <div className="title">Produto:</div>
+                        <div className="produto">
+                            <input type="text" name="input" id="produto" placeholder="Digite nome do produto" required />
+                        </div>
+                        <div className="title">Pedido:</div>
+                        <div className="pedido">
+                            <input type="number" name="input" id="email" placeholder="Insira o número do pedido" required />
+                        </div>
+                        <div className="title">Data:</div>
+                        <div className="data">
+                            <input type="date" name="input" id="email" placeholder="Insira o número do pedido" required />
+                        </div>
+                    </div>
+                </form>
+                <div className="AreaButton">
+
+                    <div class="button_left"> 
+                        <a href="/">Salvar</a>
+                    </div>
+
+                    <div class="button-right"> 
+                        <a href="login.html">Cancelar</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { PainelProducao } from '../pages/P.Producao/index';
+import { PainelProducao } from '../pages/OP/index';
+import { AProducao } from '../pages/A.Producao';
 
 export const MainRoutes = () => {
     return (
         <Routes>
-            <Route path="" element={<PainelProducao />} />
+            <Route path="/" element={<PainelProducao />} />
+            <Route path="/AProducao" element={<AProducao />} />
         </Routes>
     );
 }
